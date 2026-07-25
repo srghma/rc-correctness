63a7bb7264446b7368ab7f26a3e3840b6f7d78fd
Serhii Khoma <sergey@nordicresults.com>
2026-07-25 09:40:22 +0700

    feat: update to lean v4 -> add system files

---
+++ src/RcCorrectness.lean
@@ -0,0 +1,6 @@
+import RcCorrectness.Util
+import RcCorrectness.Ast
+import RcCorrectness.TypeSystem
+import RcCorrectness.Compiler
+import RcCorrectness.WellFormedness
+import RcCorrectness.Theorem
